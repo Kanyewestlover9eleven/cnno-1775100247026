2fceb93677abfa3368071f3db01655df4cce5fd8
DatDawg <149214606+Kanyewestlover9eleven@users.noreply.github.com>
2026-04-02 11:24:15 +0800

    Add next.config.js

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  images: { unoptimized: true },
+  trailingSlash: true,
+  basePath: '/cnno-1775100247026',
+  assetPrefix: '/cnno-1775100247026'
+};
+module.exports = nextConfig;
